19e134637422e019a179959a8a3ea1552bd566c1
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-04-12 01:09:09 -0400

    --

--- src/component/AudioPlayer.jsx
+++ src/component/AudioPlayer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect } from 'react'
+import React, { useState, useEffect, useLayoutEffect, useRef } from 'react'
 import axios from 'axios'
 import { FaPlayCircle, FaPauseCircle } from 'react-icons/fa'
 import {BsFillSkipEndFill, BsFillSkipStartFill} from 'react-icons/bs'
@@ -6,6 +6,55 @@ import AudioContainerSm from './AudioContainerSm'
 
 const AudioPlayer = (props) => {
     const { audioObjectArray, currentPlayState, setCurrentPlayState, img, setImg, masterAudio, dangerousHTML, setDangerousHTML } = props;
+
+    const canvasRef = useRef() //TODO: Make this be the canvas that is in the audioplayer component
+
+    const drawAudioVisualiser = (ctx, canvas) => {
+
+        var audio = new Audio("./victory.wav")
+
+        var context = new AudioContext();
+        var src = context.createMediaElementSource(audio);
+        var analyser = context.createAnalyser();
+
+        src.connect(analyser);
+        analyser.connect(context.destination);
+
+        analyser.fftSize = 1024;
+
+        var bufferLength = analyser.frequencyBinCount;
+        var dataArray = new Uint8Array(bufferLength);
+
+        var WIDTH = canvas.width;
+        var HEIGHT = canvas.height;
+
+        var barWidth = (WIDTH / bufferLength) * 1;
+        var barHeight;
+        var x = 0;
+
+        const renderFrame = () => {
+            requestAnimationFrame(renderFrame)
+
+            x = 0;
+
+            analyser.getByteFrequencyData(dataArray);
+            ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+            for (let i = 0; i < bufferLength; i++) {
+                barHeight = dataArray[i] / 2;
+
+                var r = 255;
+                var g = 255;
+                var b = 255;
+
+                ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+                ctx.fillRect(x, HEIGHT - barHeight, barWidth * 3, barHeight)
+
+                x += barWidth + 1;
+            }
+        }
+        renderFrame();
+    }
 
     //This allows the user to play/pause and select other tracks within the tracklist
     const handleGranularPlayback = (index, oneAudio) => { //TODO: REFACTOR THIS FUNCTION.
@@ -80,12 +129,22 @@ const AudioPlayer = (props) => {
         masterAudio.current.play();
     }
 
+    useEffect(() => {
+        const canvas = canvasRef.current
+        const ctx = canvas.getContext('2d')
+
+        //Our draw come here
+        drawAudioVisualiser(ctx, canvas)
+    }, [drawAudioVisualiser])
+
     return (
         <div className='flex items-center flex-col w-full xl:w-1/2 md:w-2/3 bg-mines-900 relative rounded-lg overflow-hidden shadow-lg'>
             <div className='flex w-full h-[250px] relative'>
                 <img src={img} className='w-[250px]' alt="" />
                 <div className="flex flex-col w-full">
-                    <div className="w-full h-1/2 bg-red-500"></div>
+                    <div className="w-full h-1/2">
+                        <canvas ref={canvasRef} className="w-full h-full bg-red-500" />
+                    </div>
                     <div className="w-full h-1/2 flex justify-center items-center gap-10 py-">
                         <BsFillSkipStartFill
                             className="text-neutral-200 hover:cursor-pointer hover:text-white"
